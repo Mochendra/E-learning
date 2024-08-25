a988346f9f59d6a2545ced19ce92db09c20acb6b
Moch. Hendra Admaja <hendraadmaja946@gmail.com>
2024-08-25 16:07:40 +0700

    perubahan lokasi

---
+++ resources/views/siswa/kelas_VIII_siswa.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kelas VIII</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Dancing+Script:wght@400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&family=Russo+One&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container"> 
+        <div class="card card-minggu">
+            <div class="card-header card-isi-siswa">
+                Minggu 1
+            </div>
+            <div class="card-body card-isi-siswa">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Buka</a>
+            </div>
+        </div>
+    </div>
+    <div class="container"> 
+        <div class="card card-minggu">
+            <div class="card-header card-isi-siswa">
+                Minggu 2
+            </div>
+            <div class="card-body card-isi-siswa">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Buka</a>
+            </div>
+        </div>
+    </div>
+</body>
